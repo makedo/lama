87842984411e24681b944e77e36c5a06c3dd4e7a
Makedo <aleander.krivous@esky.com>
2015-11-24 00:40:17 +0100

    basic configuration:  - path is hardcoded  - if there is no folder with sizes - 404   - if image ratio is not 1 - 404

--- public/index.php
+++ public/index.php
@@ -8,35 +8,36 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 $paths = [
-    'img/airlines/logos/' => __DIR__ . '/images'
+    'img/airlines/logos/' => __DIR__ . '/images/'
 ];
 
 $app->get('{path}' . '{width}' . '{sizedelimiter}' . '{height}' . '/' . '{name}.{extension}',
     function (Silex\Application $app, $path, $width, $height, $sizedelimiter, $name, $extension) use ($paths) {
 
-
-        $loadPath = '';
-        if (array_key_exists($path, $paths)) { //if there no path in valid paths
-            $loadPath = $paths[$path];
-        } else {
-            // @TODO return fallback image
-            return new \Symfony\Component\HttpFoundation\Response('Not found', 404);
-        }
-
         $sizePath = $width . $sizedelimiter . $height . '/';
-        if (! file_exists(($path . $sizePath ))) { //if there no folder with needed size
-            return new \Symfony\Component\HttpFoundation\Response('Not found', 404);
+
+        if (!file_exists(($path . $sizePath)) || !array_key_exists($path, $paths)) { //if there no folder with needed size
+            return new \Symfony\Component\HttpFoundation\Response(
+                sprintf('Path %s does not exist', $path . $sizePath),
+                404
+            );
         }
 
+        $loadPath = $paths[$path];
         $manager = new ImageManager(array('driver' => 'gd'));
         $image = $manager->make($loadPath . $name . '.' . $extension);
 
-        if (($image->getWidth() - $image->getHeight()) > 0) { //if image is not square
-            return new \Symfony\Component\HttpFoundation\Response('Not found', 404);
+        $imageAspectRatio = round($image->getWidth() / $image->getHeight(), 3);
+        $resizedAspectRatio = round($width / $height, 3);
+
+        if ( abs($imageAspectRatio - $resizedAspectRatio) > 0.01) { //if aspect ratios are different
+            return new \Symfony\Component\HttpFoundation\Response(
+                sprintf('Can\'t resize because of different aspect ratio', $sizePath),
+                500
+            );
         }
 
         $image->resize($width, $height);
-
         $savedFile = $path . $sizePath . $name . '.' . $extension;
         $image->save($savedFile);
 
@@ -48,9 +49,5 @@ $app->get('{path}' . '{width}' . '{sizedelimiter}' . '{height}' . '/' . '{name}.
     ->assert('width', '[1-9]{1}[0-9]{0,3}')
     ->assert('height', '[1-9]{1}[0-9]{0,3}')
     ->assert('extension', 'png|jpg');
-
-$app->error(function() {
-    return new \Symfony\Component\HttpFoundation\Response('Not found', 404);
-});
 
 $app->run();
